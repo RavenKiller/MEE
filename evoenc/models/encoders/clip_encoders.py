68474f94dff13e9133b49a168f1bb4ccea3b9547
raven <raven025@outlook.com>
2023-03-08 02:58:11 +0000

    update text encoding

--- evoenc/models/encoders/clip_encoders.py
+++ evoenc/models/encoders/clip_encoders.py
@@ -76,24 +76,6 @@ class CLIPEncoder(nn.Module):
                 sub_instruction = observations["sub_instruction"].int()
                 shape = sub_instruction.shape
                 sub_instruction = sub_instruction.reshape((-1, shape[-1]))
-                idx = (sub_instruction>0).any(dim=-1) # N*L, index of useful position
-                attention_mask = sub_instruction>0
-                sub_embedding = torch.zeros((shape[0]*shape[1], 512), dtype=torch.float).to(
-                    sub_instruction.device
-                )
-                self.model.encode_text(sub_instruction[idx]).float()
-                # LND -> NLD
-                sub_embedding_seq = self.sub_embedding_seq.float().permute(
-                    1, 0, 2
-                )
-                sub_embedding[idx] = sub_embedding_seq
-                sub_embedding = sub_embedding.reshape((shape[0], shape[1], sub_embedding.shape[-1]))
-                sub_mask = (sub_embedding!=0).any(dim=2)
-
-                
-                sub_instruction = observations["sub_instruction"].int()
-                shape = sub_instruction.shape
-                sub_instruction = sub_instruction.reshape((-1, shape[-1]))
                 idx = (sub_instruction > 0).any(dim=-1)  # N*L, index of useful position
                 attention_mask = sub_instruction > 0
                 sub_embedding = torch.zeros(
